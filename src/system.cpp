6429c1f44474e7ca9579214b4ad6f3b259ebae48
Sindre Froyn <sindre.froyn@gmail.com>
2020-04-30 17:07:42 +0200

    Calling LinuxParser from system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -26,13 +26,21 @@ std::string System::Kernel() { return string(); }
 float System::MemoryUtilization() { return 0.0; }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() {
+	return LinuxParser::OperatingSystem();
+}
 
-// TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+// DONE: Return the number of processes actively running on the system
+int System::RunningProcesses() {
+	return LinuxParser::RunningProcesses();
+}
 
-// TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+// DONE: Return the total number of processes on the system
+int System::TotalProcesses() {
+	return LinuxParser::TotalProcesses();
+}
 
-// TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+// DONE: Return the number of seconds since the system started running
+long int System::UpTime() {
+	return LinuxParser::UpTime();
+}
