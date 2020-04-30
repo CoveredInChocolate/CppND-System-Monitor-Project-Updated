27e563ef2189d105901d45119e0447cecf4a8b6d
CoveredInChocolae <sindre.froyn@gmail.com>
2020-04-30 21:48:18 +0000

    Fixing git

--- src/system.cpp
+++ src/system.cpp
@@ -7,6 +7,7 @@
 #include "process.h"
 #include "processor.h"
 #include "system.h"
+#include "linux_parser.h" //Added so LinuxParser:: works
 
 using std::set;
 using std::size_t;
@@ -25,26 +26,22 @@ std::string System::Kernel() { return string(); }
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
-// TODO: Return the operating system name
+// DONE: Return the operating system name
 std::string System::OperatingSystem() {
-	//return LinuxParser::OperatingSystem();
-	return "SindreTest-22";
+	return LinuxParser::OperatingSystem();
 }
 
 // DONE: Return the number of processes actively running on the system
 int System::RunningProcesses() {
-	//return LinuxParser::RunningProcesses();
-	return 200;
+	return LinuxParser::RunningProcesses();
 }
 
 // DONE: Return the total number of processes on the system
 int System::TotalProcesses() {
-	//return LinuxParser::TotalProcesses();
-	return 500;
+	return LinuxParser::TotalProcesses();
 }
 
 // DONE: Return the number of seconds since the system started running
 long int System::UpTime() {
-	//return LinuxParser::UpTime();
-	return 2000;
+	return LinuxParser::UpTime();
 }
