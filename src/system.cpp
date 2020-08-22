3279b002e7e67ba91cca77fab792f04e22c5d00a
Sindre Froyn <sindre.froyn@gmail.com>
2020-08-23 01:40:04 +0200

    Reverting

--- src/system.cpp
+++ src/system.cpp
@@ -16,7 +16,7 @@ using std::vector;
 
 // TODO: Return the system's CPU
 // Test dummy variable - test2
-Processor& System::Cpu() { return cpu_.Utilization(); }
+Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() { return processes_; }
