c8690f24c8ba02ba335d1b832a827410962ef01f
Sindre Froyn <sindre.froyn@gmail.com>
2020-08-23 18:32:42 +0200

    Sorting 'processes_' in system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -27,7 +27,10 @@ System::System() {
 Processor& System::Cpu() { return cpu_; }
 
 // DONE: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process>& System::Processes() {
+	sort(processes_.begin(), processes_.end());
+	return processes_;
+}
 
 // DONE: Return the system's kernel identifier (string)
 std::string System::Kernel() {
