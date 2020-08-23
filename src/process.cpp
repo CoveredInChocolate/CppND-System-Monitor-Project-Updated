71b7dc9ebf952bb33bebdbe1baa2bacbd04bfa15
Sindre Froyn <sindre.froyn@gmail.com>
2020-08-23 18:41:03 +0200

    Added uptime for processes

--- src/process.cpp
+++ src/process.cpp
@@ -41,7 +41,10 @@ string Process::User() {
 }
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+long int Process::UpTime() {
+  long timeSeconds = LinuxParser::UpTime(Pid());
+  return timeSeconds;
+}
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
