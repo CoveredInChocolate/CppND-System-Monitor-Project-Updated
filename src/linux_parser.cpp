f9347addbb7e84f855b6cdad5fc96b2ce744f7b2
Sindre Froyn <sindre.froyn@gmail.com>
2020-08-23 18:44:12 +0200

    Added uptime for processes - adjusted for clock ticks

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -285,8 +285,8 @@ long LinuxParser::UpTime(int pid) {
 		std::istringstream linestream(line);
 		linestream >> v1 >> v2 >> v3 >> v4 >> v5 >> v6 >> v7 >> v8 >> v9 >> v10 >> v11 >> v12 >> v13 >> v14 >> v15 >> v16 >> v17 >> v18 >> v19 >> v20 >> v21 >> v22;
 	}
-	long returnValue = std::stol(v22);
+	//long returnValue = std::stol(v22);
 	// Correcting from clock ticks to seconds
-	//long returnValue = std::stol(v22)/sysconf(_SC_CLK_TCK);
+	long returnValue = std::stol(v22)/sysconf(_SC_CLK_TCK);
 	return returnValue;
 }
